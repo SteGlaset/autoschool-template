24ba45985d83e1a012545ec2410be3b3f9571a12
SteGlaset <khlybov.dania@yandex.ru>
2023-04-04 17:55:41 +0300

    init: start automsk, make html markup

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
 import { defineConfig } from 'vite'
 import pugPlugin from 'vite-plugin-pug'
+import path from "path";
+const {sync} = require("glob");
 
 const { resolve } = require('path')
 
 export default defineConfig({
-  plugins: [pugPlugin(undefined, { pagesUrl: './pages/' })],
+  plugins: [pugPlugin({pretty: true}, { pagesUrl: './branches/' })],
 
-  server: { port: 9001 },
-  optimizeDeps: {
-    // include: ['node_modules/gsap/ScrollTrigger'],
-  },
+  server: { port: 8083 },
 
   build: {
     rollupOptions: {
       input: {
-        // need a better way to template
         main: resolve(__dirname, 'index.html'),
-        page: resolve(__dirname, 'pages/page.html'),
-        page2: resolve(__dirname, 'pages/page2.html'),
+        ...sync('./branches/**.html').reduce(function(obj, el){
+          obj['branches/' + path.parse(el).name] = el;
+          return obj
+        },{})
       },
     },
   },
